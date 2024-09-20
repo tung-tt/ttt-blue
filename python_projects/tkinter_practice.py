23e6e3ccba66c831490d9ec45eac8e24643bd88b
Tung T. Tran <yungtungtt@gmail.com>
2024-09-20 18:16:06 -0400

    added label

--- python_projects/tkinter_practice.py
+++ python_projects/tkinter_practice.py
@@ -1,10 +1,14 @@
 from tkinter import *
 # Tkinter practice
-# Window Initialize 
+
+# Window Initialize, size set
 root = Tk() # Creates a root widget
 root.title('Tungo GUI')
 root.configure(background = 'grey')
 root.minsize(150,150)
 root.maxsize(500,500)
+# Label widget
+text = Label(root, text='Hello World')
+text.pack()
 # Starts GUI window
 root.mainloop()
