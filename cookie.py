e01fc1b74b4cf21dba5b940a397902f038282d2f
Tung T. Tran <yungtungtt@gmail.com>
2024-09-20 14:27:36 -0400

    fixed loop

--- cookie.py
+++ cookie.py
@@ -2,18 +2,22 @@ from time import sleep
 # Cookie Program
 cookies_amount = 0
 
-def cookie_start():
+def cookie_start(cookies_goal):
     global cookies_amount
-    while cookies_amount < 5:
+    while cookies_amount <= cookies_goal:
         sleep(1)
-        print(f'You have {cookies_amount} cookies.')
+        print(f'You now have {cookies_amount} cookies.')
         cookies_amount += 1
+    print('--🍪 COOKIES DISPENSED--')
 
 # Main Loop
 while True:
-    start = input("Cookies, start?: ").lower()
+    print('-- WELCOME TO 🍪 --')
+    start = input("Want some cookies?: ").lower()
     if start == "yes" or start =="go":
+        cookies_goal = int(input("How many cookies do you want?: "))
+        print('--🍪 INITIALIZED--')
+        cookie_start(cookies_goal)
         break
     continue
 
-cookie_start()
