e6bdc5cef53d6946ab1616c5dddf3dbd05eb3d35
Tung T. Tran <ttungjr4@gmail.com>
2024-09-21 01:06:56 -0400

    imported time to tkinter prac

--- python_projects/tkinter_practice.py
+++ python_projects/tkinter_practice.py
@@ -1,14 +1,18 @@
 from tkinter import *
+from time import sleep
 # Tkinter practice
 
 # Window Initialize, size set
 root = Tk() # Creates a root widget
 root.title('Tungo GUI')
-root.configure(background = 'grey')
 root.minsize(150,150)
 root.maxsize(500,500)
 # Label widget
-text = Label(root, text='Hello World')
-text.pack()
-# Starts GUI window
-root.mainloop()
+cookie_amount = 0
+while True:
+    sleep(1)
+    cookie_amount += 1
+    text = Label(root, text=cookie_amount)
+    text.pack()
+    # Starts GUI window
+    root.mainloop()
