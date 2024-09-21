2ed841cf08c9ac527793169b0a7423e4a971b201
Tung T. Tran <yungtungtt@gmail.com>
2024-09-21 12:20:54 -0400

    accept/reject lists

--- python_projects/cookie_dispenser.py
+++ python_projects/cookie_dispenser.py
@@ -1,14 +1,20 @@
 from time import sleep
 # Imported sleep for 1 second delays
 # 🍪 Dispenser Program
+# Variables & Lists
+counter = 0
 cookies_amount = 0
+accept = ['yes', 'sure', 'fine', 'ok', 'cookies']
+reject = ['no', 'stop', 'im good']
+# Start
 print('-- WELCOME TO 🍪 --')
 sleep(0.5)
 # Cookies Incremental Functon
 def cookie_start(cookies_goal):
     # Access cookies_amount from outside
     global cookies_amount
-    while cookies_amount <= cookies_goal:
+    cookies_copy = cookies_amount
+    while cookies_amount <= (cookies_copy + cookies_goal):
         sleep(1)
         print(f'You now have {cookies_amount} cookies.')
         cookies_amount += 1
@@ -16,21 +22,25 @@ def cookie_start(cookies_goal):
 
 # Main Loop
 while True:
-    start = input("Want some cookies?: ").lower()
+    match counter >= 1:
+        case True:
+            start = input("Want some more cookies?: ").lower()
+        case _:
+            start = input("Want some cookies?: ").lower()
     # Switch case instead of if statements, checks corresponding strings from start input
-    match start:
-        case 'yes':
+    if start in accept:
+            counter += 1
             cookies_goal = int(input("How many cookies do you want?: "))
             print('--🍪 INITIALIZED--')
             # Calls cookies_start function with cookies_goal as limit
             cookie_start(cookies_goal)
-        case 'stop':
-            print("Fine, get outta here")
-            sleep(0.5)
-            print('--🍪 ENDING--')
-            break
-        case _:
-            print("Oh, ok... but")
+    elif start in reject:
+        print("Fine, get outta here")
+        sleep(0.5)
+        print('--🍪 ENDING--')
+        break
+    else:
+        print("I don't understand, but...")
     continue
 
 sleep(0.5)
--- python_projects/tkinter_practice.py
+++ python_projects/tkinter_practice.py
@@ -4,11 +4,13 @@ from tkinter import *
 # Window Initialize, size set
 root = Tk() # Creates a root widget
 root.title('Tungo GUI')
-root.configure(background = 'grey')
+root.configure(background = 'white')
 root.minsize(150,150)
 root.maxsize(500,500)
 # Label widget
 text = Label(root, text='Hello World')
 text.pack()
+text2 = Label(root, text = '🍪 Cookie Dispenser')
+text2.pack()
 # Starts GUI window
 root.mainloop()
